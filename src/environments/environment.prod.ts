decfb6f4cfa812a94cee5a8df36a87f24bab7f75
Jiahian <chee0132@e.ntu.edu.sg>
2019-11-29 12:24:57 +0800

    firebase setup and google signin authentication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCIItao1q5VKOL2sPpI0UmZ2IVZ7g88E-I",
+    authDomain: "my-dev-app-1d916.firebaseapp.com",
+    databaseURL: "https://my-dev-app-1d916.firebaseio.com",
+    projectId: "my-dev-app-1d916",
+    storageBucket: "my-dev-app-1d916.appspot.com",
+    messagingSenderId: "198539337130",
+    appId: "1:198539337130:web:5b04798fa552d8363b9fd3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCIItao1q5VKOL2sPpI0UmZ2IVZ7g88E-I",
+    authDomain: "my-dev-app-1d916.firebaseapp.com",
+    databaseURL: "https://my-dev-app-1d916.firebaseio.com",
+    projectId: "my-dev-app-1d916",
+    storageBucket: "my-dev-app-1d916.appspot.com",
+    messagingSenderId: "198539337130",
+    appId: "1:198539337130:web:5b04798fa552d8363b9fd3"
+  }
 };
 
 /*
